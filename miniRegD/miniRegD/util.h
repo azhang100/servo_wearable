275c89db35cf982fe3a07751c840d6fe099cc589
brianhaimowitz <113564455+brianhaimowitz@users.noreply.github.com>
2022-10-26 14:35:10 -0400

    Updated pressure conversion

---
+++ miniRegD/miniRegD/util.h
@@ -0,0 +1,73 @@
+#ifndef Util_h
+#define Util_h
+
+#include "Arduino.h"
+#include <Wire.h>
+
+
+// ============== PRINTING ==========
+
+#define START_DELIM '['
+#define END_DELIM ']'
+#define SPLIT_DELIM '='
+
+#define DBSERIAL Serial // debugging
+
+#define PRINT1(a)         DBSERIAL.println(F(a))
+#define PRINT2(a,b)       DBSERIAL.print  (F(a)); DBSERIAL.println(b)
+#define PRINT3(a,b,c)     DBSERIAL.print  (F(a)); DBSERIAL.print  (b); DBSERIAL.println(F(c))
+#define PRINT3NL(a,b,c)   DBSERIAL.print  (F(a)); DBSERIAL.print  (b); DBSERIAL.print(F(c))
+#define PRINT4(a,b,c,d)   DBSERIAL.print  (F(a)); DBSERIAL.print  (b); DBSERIAL.print  (F(c)); DBSERIAL.println(d)
+#define PRINT4NL(a,b,c,d) DBSERIAL.print  (F(a)); DBSERIAL.print  (b); DBSERIAL.print  (F(c)); DBSERIAL.print  (d)
+
+#define SLOWPRINT1(a)         if (isSlowPrint){ PRINT1(a); }
+#define SLOWPRINT2(a,b)       if (isSlowPrint){ PRINT2(a,b); }
+#define SLOWPRINT3(a,b,c)     if (isSlowPrint){ PRINT3(a,b,c); }
+#define SLOWPRINT4(a,b,c,d)   if (isSlowPrint){ PRINT4(a,b,c,d); }
+#define SLOWPRINT4NL(a,b,c,d) if (isSlowPrint){ PRINT4NL(a,b,c,d); }
+
+bool isSlowPrint = true;
+
+void resetSlowPrint(){
+  static long lastTime = millis();
+  if ((millis()-lastTime) > 1000){
+    lastTime = millis();
+    isSlowPrint = true;
+  } else {
+    isSlowPrint = false;
+  }
+}
+
+// =============== LED ================
+
+#define PIN_LED PC13
+#define LED_DURATION 200
+
+void setupUtil(){
+  pinMode(PIN_LED, OUTPUT);
+  DBSERIAL.begin(9600); // debugging
+  PRINT1("Debug Serial start");
+  Wire.begin();
+  
+}
+
+void loopUtil(){
+  digitalWrite(PIN_LED,millis()%(LED_DURATION*2) > LED_DURATION);
+  // PRINT1("loop");
+}
+
+// =============== analogread to millivolt ================
+
+static const long V_DIV = 11000; // 1M / 100K
+
+// overflow 2,147,483,647
+// TODO: use long instead of float
+int analogReadToMillivolt(int n){
+  // performs calculation in float to prevent overflow
+  float res = float(n);
+  res = res*5120*V_DIV/1024/1024;
+  return int(res);
+}
+
+
+#endif

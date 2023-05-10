4c9871bc9fe6ed081a1ff042be1f5f9b76d3375c
brianhaimowitz <113564455+brianhaimowitz@users.noreply.github.com>
2023-05-10 15:10:15 -0400

    Updated PID and CO2 Callibration

--- blowerTest/blowerTest/sweep.h
+++ blowerTest/blowerTest/sweep.h
@@ -8,28 +8,37 @@ String info = "";
 int numEnds = 0;
 String egco2 = "";
 float inEGCO2;
-float setPointSweep = 40;
-float KpSweep = .03;
-float KiSweep = 0;
-float KdSweep = 0.001;
+float setPointSweep = 20;
+//CURRENT
+float KpSweep = .02;
+float KiSweep = 0.000002;
+float KdSweep = 0;
+//OLD
+//float KpSweep = .015;
+//float KiSweep = 0.000003;
+//float KdSweep = 0;
 float currentTimeSweep;
 float previousTimeSweep;
 float cumErrorSweep;
 float sweep = 1;
-float rawLowEGCO2 = 19.8;
-float rawHighEGCO2 = 26.3;
-float referenceLowEGCO2 = 23.9;
-float referenceHighEGCO2 = 35.9;
+float rawLowEGCO2 = 13.4;
+float rawHighEGCO2 = 20;
+float referenceLowEGCO2 = 26;
+float referenceHighEGCO2 = 38.2;
 float rateErrorSweep;
 float firstTimeSweep;
 float errorSweep = 0;
 float testTime;
+float outputSweep;
 bool buildup = true;
+bool ten = true;
+bool twenty = true;
+bool forty = true;
+
 
 void setupSweep(){
   Serial2.begin(9600);
   firstTimeSweep = millis();
-  testTime = millis();
   float currentTimeSweep = millis() - firstTimeSweep;
   float previousTimeSweep = currentTimeSweep;
   float cumErrorSweep = 0;
@@ -61,30 +70,42 @@ void getSweep(){
   DBSERIAL.print("[egco2=");
   //DBSERIAL.println(egco2);
   inEGCO2 = egco2.toFloat() * 7.6;
-  inEGCO2 = ((inEGCO2-rawLowEGCO2)*(referenceHighEGCO2-referenceLowEGCO2))/(rawHighEGCO2-rawLowEGCO2)+referenceLowEGCO2;
+  if(inEGCO2 < 2.4){
+    inEGCO2 = ((5-0)/(2.4+16.42))*inEGCO2 + (((5-0)/(2.4+16.42))*16.42);
+  }
+  else{
+    inEGCO2 = ((inEGCO2-rawLowEGCO2)*(referenceHighEGCO2-referenceLowEGCO2))/(rawHighEGCO2-rawLowEGCO2)+referenceLowEGCO2;
+  }
+  if(inEGCO2 < 0){
+    inEGCO2 = 0;
+  }
   DBSERIAL.print(inEGCO2);
   DBSERIAL.println("]");
 
 }
 
 float piSweep(){
+    //changeSet();
     previousTimeSweep = currentTimeSweep;
     currentTimeSweep = millis();
     float elapsedTimeSweep = currentTimeSweep - previousTimeSweep;
     float lastErrorSweep = errorSweep;
     float errorSweep = -setPointSweep + (inEGCO2);
-    rateErrorSweep = (errorSweep - lastErrorSweep)/elapsedTimeSweep;
-    cumErrorSweep += errorSweep * elapsedTimeSweep;
-    if(elapsedTimeSweep > 10){
-      KiSweep = 0.000006;
-    }
-    float outputSweep = KpSweep * errorSweep + KiSweep * cumErrorSweep + KdSweep * rateErrorSweep;
-    if(outputSweep <  inEGCO2 * 0.013){
-      outputSweep =  inEGCO2 * 0.013;
-      if(outputSweep < 0.1){
-        outputSweep = 0.1;
+//    if(abs(errorSweep) > 0.3){
+      rateErrorSweep = (errorSweep - lastErrorSweep)/elapsedTimeSweep;
+      cumErrorSweep += errorSweep * elapsedTimeSweep;
+      outputSweep = KpSweep * errorSweep + KiSweep * cumErrorSweep + KdSweep * rateErrorSweep;
+      if(outputSweep <  inEGCO2 * 0.015){
+        outputSweep =  inEGCO2 * 0.015;
+        if(outputSweep < 0.1){
+          outputSweep = 0.1;
+        }
       }
+//    }
+    if(cumErrorSweep < -1 & errorSweep < 0){
+      cumErrorSweep = 0;
     }
+    outputSweep = 2;
   return outputSweep;
 }
 
